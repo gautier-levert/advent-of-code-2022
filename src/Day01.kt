433739d276443f861f61e8a8dfa9af8322fe0b42
Gautier Levert <gautier@levert.xyz>
2022-12-01 16:15:30 +0100

    Add day 1 answer

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,34 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-        TODO()
+        return input.asSequence()
+            .chunkByPredicate { it.isEmpty() }
+            .maxOf { list ->
+                list.asSequence()
+                    .map { it.toInt(10) }
+                    .sum()
+            }
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        return input.asSequence()
+            .chunkByPredicate { it.isEmpty() }
+            .map { list ->
+                list.asSequence()
+                    .map { it.toInt(10) }
+                    .sum()
+            }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
-    // check(part2(testInput) == 5)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println("part 1 answer: ${part1(input)}")
-    // println("part 2 answer: ${part2(input)}")
+    println("part 2 answer: ${part2(input)}")
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,25 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+
+fun <T : Any?> Sequence<T>.chunkByPredicate(
+    includeSeparatorElement: Boolean = false,
+    predicate: (T) -> Boolean,
+): Sequence<List<T>> {
+    return sequence {
+        var current = mutableListOf<T>()
+        forEach { elt ->
+            if (predicate(elt)) {
+                if (includeSeparatorElement) {
+                    current.add(elt)
+                }
+                yield(current)
+                current = mutableListOf()
+            } else {
+                current.add(elt)
+            }
+        }
+        yield(current)
+    }
+}
