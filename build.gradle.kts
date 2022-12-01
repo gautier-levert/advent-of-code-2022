9e0d87162cfbed128cd5f8ec3e3ad3d0c98d44d8
Gautier Levert <gautier@levert.xyz>
2022-12-01 15:28:06 +0100

    Initial project structure

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.7.22"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    sourceSets {
+        main {
+            java.srcDirs("src")
+        }
+    }
+}
