f3eba8f233aef052a596eebb6a6320f469a72ab1
Gautier Levert <gautier@levert.xyz>
2022-12-02 10:35:49 +0100

    Add day 2 input

---
+++ src/Day02.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        TODO()
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 15)
+    // check(part2(testInput) == ???)
+
+    val input = readInput("Day02")
+    println("part 1 answer: ${part1(input)}")
+    // println("part 2 answer: ${part2(input)}")
+}
